e8f3b6e5a6f7807b876b743f89e3dd38a0caad62
tom <tom@tom-K50AF.(none)>
2013-05-16 16:24:26 +0200

    Added getJobShopTime implemenattion

--- SystemControllerSim/src/pl/agh/miss/SimulatedController.java
+++ SystemControllerSim/src/pl/agh/miss/SimulatedController.java
@@ -90,14 +90,171 @@ public class SimulatedController {
 	}
 
 	/**
-	 * TODO TOMEK
 	 * 
 	 * @return
 	 */
 	private long getJobShopTime() {
-		Random rand = new Random();
-		long result = 10000 + rand.nextInt(100000);
-		return result;
+		List<Task> taskList = plan.getTasksList();
+		// key: job, value: pos on jobsTimeList
+		Map<Integer, Integer> jobsListPosMap = new HashMap<>();
+		// list of (stop start) times of each job
+		List<List<Long>> jobsTimesList = new ArrayList<>();
+		// list of (stop start) times of jobs on each machine
+		List<List<Long>> machineSlotsList = new ArrayList<>();
+		int i = 0;
+		int randJobId = 0;
+		for (int jobId : timeTransitions.keySet()) {
+			jobsListPosMap.put(jobId, i);
+			ArrayList<Long> tmpList = new ArrayList<>();
+			jobsTimesList.add(i, tmpList);
+			++i;
+			randJobId = jobId;
+		}
+		// do all list have this same size?
+		int machineCount = timeTransitions.get(randJobId).size();
+		// do machines number starts from 0 i numeracja jest ciagla?
+		for (int it = 0; it < machineCount; ++it) {
+			ArrayList<Long> tmpList = new ArrayList<>();
+			tmpList.add((long) 0);
+			tmpList.add((long) 0);
+			machineSlotsList.add(tmpList);
+		}
+
+		for (Task task : taskList) {
+			int jobId = task.getJobId();
+			int machineId = task.getMachineId();
+			long time = 0;
+			for (PassTime passTime : timeTransitions.get(jobId)) {
+				if (passTime.getMachineId() == machineId) {
+					time = passTime.getTime();
+					break;
+				}
+			}
+			if (time == 0) {
+				System.out.println("0 time break");
+				continue;
+			}
+			List<Long> currentJobTimes = jobsTimesList.get(jobsListPosMap.get(jobId));
+			List<Long> currentMachineSlots = machineSlotsList.get(machineId);
+			// try to match gap before checked slot (starts from 2nd slot)
+			int insertSlot = -1;
+			long startTime = 0;
+			for (int slotIt = 2; slotIt < currentMachineSlots.size(); slotIt += 2) {
+				int prevStart = slotIt - 2;
+				int prevStop = slotIt - 1;
+				int start = slotIt;
+				int stop = slotIt + 1;
+				if (currentMachineSlots.get(start) - currentMachineSlots.get(prevStop) >= time) {
+					long tmpStartTime = findNoConflictStartTime(currentJobTimes, currentMachineSlots.get(prevStop),
+							currentMachineSlots.get(start), time);
+					if (tmpStartTime != -1) {
+						startTime = tmpStartTime;
+						insertSlot = slotIt;
+						break;
+					}
+				}
+			}
+			// all slots are free (0 and 1 - empty 0)
+			System.out.printf("job: %d, machine:  %d", jobId, machineId);
+			if (currentMachineSlots.size() == 2 && insertSlot == -1) {
+
+				startTime = findFirstAvaiableStartTime(currentJobTimes, 0, time);
+				currentMachineSlots.add(2, startTime);
+				currentMachineSlots.add(3, startTime + time);
+				insertBeforeFirstSmallerValue(currentJobTimes, startTime, time);
+				System.out.println(" ,start: " + startTime + ", stop:  " + (startTime + time));
+				System.out.println("empty");
+			} else {
+				if (insertSlot == -1) {
+					int curMachSlotsSizeBeforeInsert = currentMachineSlots.size();
+					startTime = findFirstAvaiableStartTime(currentJobTimes,
+							currentMachineSlots.get(curMachSlotsSizeBeforeInsert - 1), time);
+					currentMachineSlots.add(curMachSlotsSizeBeforeInsert, startTime);
+					currentMachineSlots.add(curMachSlotsSizeBeforeInsert + 1, startTime + time);
+					insertBeforeFirstSmallerValue(currentJobTimes, startTime, time);
+					System.out.println(" ,start: " + startTime + ", stop:  " + (startTime + time));
+					System.out.println("no match - end");
+				}
+				// proper free slot
+				else {
+
+					currentMachineSlots.add(insertSlot, startTime);
+					currentMachineSlots.add(insertSlot + 1, startTime + time);
+					insertBeforeFirstSmallerValue(currentJobTimes, startTime, time);
+					System.out.println(" ,start: " + startTime + ", stop:  " + (startTime + time));
+					System.out.println("proper");
+				}
+			}
+
+		}
+		long max = 0;
+		for (List<Long> tmpList : machineSlotsList) {
+			long tmpMax = tmpList.get(tmpList.size() - 1);
+			max = Math.max(max, tmpMax);
+		}
+		// Random rand = new Random();
+		// long result = 10000 + rand.nextInt(100000);
+		return max;
+	}
+
+	/**
+	 * Find no conflict slot.
+	 * 
+	 * @return -1 is slot is not matched
+	 */
+	private long findNoConflictStartTime(List<Long> list, long start, long stop, long time) {
+		long tmpStart = findFirstAvaiableStartTime(list, start, time);
+		if (tmpStart + time <= stop) {
+			return tmpStart;
+		}
+		return -1;
+	}
+
+	private long findFirstAvaiableStartTime(List<Long> list, long start, long time) {
+		if (list.size() == 0) {
+			return start;
+		}
+		long startResult = list.get(list.size() - 1);
+		// if last stop from list is before start return start
+		if (startResult <= start) {
+			return start;
+		}
+		int it = 0;
+		// pass all task which ends before given start
+		while (list.get(it + 1) < start) {
+			it += 2;
+		}
+		// it fits before first
+		if (it == 0 && list.get(0) >= time) {
+			return start;
+		}
+
+		// there is one task which ends after given start time so -2
+		// search for slot starting from last slot which ends before given time
+		for (; it < list.size() - 2; it += 2) {
+			if (list.get(it + 2) - list.get(it + 1) >= time) {
+				startResult = list.get(it + 1);
+				// TODO - no conflict?
+				break;
+			}
+		}
+
+		return startResult;
+	}
+
+	private void insertBeforeFirstSmallerValue(List<Long> currentJobTimes, long startTime, long time) {
+		int insertJobSlot = 0;
+		for (int it = 0; it < currentJobTimes.size(); it += 2) {
+			insertJobSlot = it;
+			if (currentJobTimes.get(it) > startTime) {
+				break;
+			}
+			if (it + 2 == currentJobTimes.size()) {
+				insertJobSlot = currentJobTimes.size();
+			}
+		}
+		currentJobTimes.add(insertJobSlot, startTime);
+		currentJobTimes.add(insertJobSlot + 1, startTime + time);
 	}
 
 	private void sendResult(long result, Delivery delivery) throws UnsupportedEncodingException, IOException {
